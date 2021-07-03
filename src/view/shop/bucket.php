802c2f5ec2d18fbd50c0ee49c7345c9a61542192
Massil Taguemout <massiltag@icloud.com>
2021-07-03 03:05:58 +0200

    cart button styling

--- src/view/shop/bucket.php
+++ src/view/shop/bucket.php
@@ -70,6 +70,11 @@
                 background: none;
             }
 
+            .cart-btn {
+                float: right;
+                margin-left: 5px;
+            }
+
         </style>
 
         <script>
@@ -85,7 +90,7 @@
         </script>
     </head>
 
-    <body class="container" <!--onload="multiplyNode(document.getElementsByClassName('col-md-12')[0], 3, true);" -->
+    <body class="container"> <!--onload="multiplyNode(document.getElementsByClassName('col-md-12')[0], 3, true);" -->
 
         <h3>Panier de <?php echo $_SESSION["fname"] ?></h3>
         <div class="py-5">
@@ -101,14 +106,29 @@
                             <h5 class="card-title product-name">'. $key .'</h5>
                             <small class="text-muted quantity">'. $redis->hGet('panier', $key) .'</small>
                             <p class="card-text product-description">Petite description du produit, c\'est très bon et pas du tout cher, achetez le. Merci bien.</p>
-                            <a href="index.php?ctl=bucket&action=decrement&product='. $key .'" class="btn btn-primary submit">-</a>
-                            <a href="index.php?ctl=bucket&action=increment&product='. $key .'" class="btn btn-success submit">+</a>
-                            <a href="index.php?ctl=bucket&action=delete&product='. $key .'" class="btn btn-del btn-primary submit">
+                            
+                            
+                            <a href="index.php?ctl=bucket&action=increment&product='. $key .'" class="btn btn-success cart-btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                                  <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+                                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                                </svg>
+                            </a>
+                            
+                            <a href="index.php?ctl=bucket&action=decrement&product='. $key .'" class="btn btn-primary cart-btn"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash" viewBox="0 0 16 16">
+                                  <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
+                                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                                </svg>
+                            </a>
+                            
+                            <a href="index.php?ctl=bucket&action=delete&product='. $key .'" class="btn btn-del btn-primary cart-btn">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                 </svg>
                             </a>
+                            
                         </div>
                     </div>';
                     }
